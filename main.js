742302789c7c364841772b2ed5de9d3816837f34
Emil Placheta <emil.minoru@gmail.com>
2019-04-07 13:47:42 +0100

    map, sort and reduce

--- main.js
+++ main.js
@@ -14,16 +14,17 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 //basic for loop....
 for (let i = 0; i < companies.length; i++) {
-  console.log(companies[i].name);
+  //console.log(companies[i].name);
 }
 
 // ...and no with forEach
 
 companies.forEach(function(company) {
-  console.log(company);
+  //console.log(company);
 });
 /////////////////////////////////////////////////////////////////////
-//filter 'filters' out data from an array and returns all that match//
+//filter 'filters' out data from an array and returns all that match
+// creates new array//
 /////////////////////////////////////////////////////////////////////
 
 // basic for loop....
@@ -51,7 +52,7 @@ const over21 = ages.filter(function(age) {
 
 const canDrink = ages.filter(age => age >= 18);
 
-console.log(canDrink);
+//console.log(canDrink);
 
 //filter retail companies:
 let retailCompanies = companies.filter(retail => retail.catergory === 'Retail');
@@ -64,12 +65,12 @@ let eightiesCompanies = companies.filter(year => year.start < 1990);
 
 let atleast10 = companies.filter(company => (company.end - company.start >= 10));
 
-console.log(retailCompanies);
-console.log(eightiesCompanies);
-console.log(atleast10);
+//console.log(retailCompanies);
+//console.log(eightiesCompanies);
+//console.log(atleast10);
 
 /////////////////////////////////////////////////////////////////
-//map 'gets' data from an array together (creates new array)//
+//map 'gets' data from an array (creates new array)//
 /////////////////////////////////////////////////////////////////
 
 //create array of company names
@@ -78,5 +79,80 @@ const companyNames = companies.map(company => company.name);
 
 console.log(companyNames);
 
-// sort
-// reduce
+const testCompany = companies.map(function(company) {
+  return `Company name is: ${company.name}`
+})
+
+const testCompany2 = companies.map(company => `category of company: ${company.catergory}`)
+
+const testCompany3 = companies.map(function(company) {
+  return `${company.name} was ${company.end - company.start} years on the marker`
+})
+
+
+
+const multipliedByItself = ages.map(age => age * age);
+const squareAges = ages.map(age => Math.sqrt(age));
+
+const faffingMap = ages
+  .map(age => age * 2)
+  .map(age => age / 2)
+  .map(test => test - 10)
+
+
+
+//console.log(testCompany);
+//console.log(testCompany2);
+//console.log(testCompany3);
+
+//console.log(multipliedByItself);
+//console.log(squareAges);
+//console.log(faffingMap);
+
+
+
+/////////////////////////////////////////////////////////////////
+//sort //
+/////////////////////////////////////////////////////////////////
+
+const sortedCompanies = companies.sort(function(c1, c2) {
+  if(c1.start > c2.start) {
+    return 1;
+  } else {
+    return -1
+  }
+});
+
+const sortedAges = ages.sort((a,b) => a > b ? 1 : -1);
+
+console.log(sortedCompanies);
+console.log(sortedAges);
+
+
+
+// reduce makes new array
+
+let ageSum = 0;
+
+for (let i = 0; i < ages.length; i++) {
+    ageSum += ages[i]
+}
+
+const reduceAgeSum = ages.reduce((total, age) => total + age, 0);
+//total = its like creating an empty variable to add things to
+//age = arbitrary name for items in the array to be added
+//0 = base value to which we will start adding
+
+const reduceTimeAllCompaniesWereOnTheMarket = companies.reduce(function (total, company) {
+  return total + (company.end - company.start)
+}, 0);
+
+const reduceTimeAllCompaniesWereOnTheMarketShortHand = companies.reduce( (total, time) => total + (time.end - time.start),0)
+
+
+
+
+console.log(ageSum);
+console.log(reduceAgeSum);
+console.log(reduceTimeAllCompaniesWereOnTheMarket);
+console.log(reduceTimeAllCompaniesWereOnTheMarketShortHand);
